ae4f9fc02cc1d6848b2b9dc9b631428939ac1ef6
Anna Drobysh <ann.drobysh@yandex.by>
2021-05-03 23:03:02 +0200

    Create new class for sessions management

--- test/test_group_add.py
+++ test/test_group_add.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-from fixture.app_add_group import AddGroupApp
+from fixture.Application import Application
 
 
 @pytest.fixture
 def app(request):
-    fixture = AddGroupApp()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(gr_name="Test group", gr_header="test header", gr_footer="test footer"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_group_empty(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(gr_name="", gr_header="", gr_footer=""))
-    app.logout()
+    app.session.logout()
