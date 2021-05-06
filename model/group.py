5e0a5e760cc454c8d264d79f070d583b356d39e9
Anna Drobysh <ann.drobysh@yandex.by>
2021-05-06 10:31:54 +0200

    Change "Edit group" test.

--- model/group.py
+++ model/group.py
@@ -1,10 +1,7 @@
 class Group:
-    def __init__(self, gr_title = "", gr_name = "", gr_header = "", gr_footer = "", new_gr_name = "", new_gr_header = "", new_gr_footer = ""):
+    def __init__(self, gr_title = "", gr_name = "", gr_header = "", gr_footer = ""):
         self.gr_name = gr_name
         self.gr_header = gr_header
         self.gr_footer = gr_footer
         self.gr_title = gr_title
-        self.new_gr_name = new_gr_name
-        self.new_gr_header = new_gr_header
-        self.new_gr_footer = new_gr_footer
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_edit_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit(Group(gr_title="abc", new_gr_name="changed", new_gr_header="header changed", new_gr_footer="footer changed"))
+    app.group.edit(Group(gr_title="changed 1", gr_name="changed 2", gr_header="header changed 2", gr_footer="footer changed 2"))
     app.session.logout()
