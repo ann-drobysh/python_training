f52f2887c18e7ed7ce51b9d4cd71fbf6059bde34
Anna Drobysh <ann.drobysh@yandex.by>
2021-05-13 18:02:20 +0200

    Change logout method

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,12 +3,10 @@ from model.group import Group
 
 def test_edit_first_group_name(app):
     app.group.edit_first_group(Group(gr_name="Name changed"))
-    app.session.logout()
 
 
 def test_edit_first_group_header(app):
     app.group.edit_first_group(Group(gr_header="header changed"))
-    app.session.logout()
 
 
 def test_edit_first_group_footer(app):
--- test/test_group_add.py
+++ test/test_group_add.py
@@ -4,7 +4,6 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(gr_name="Test group", gr_header="test header", gr_footer="test footer"))
-    app.session.logout()
 
 def test_add_group_empty(app):
     app.group.create(Group(gr_name="", gr_header="", gr_footer=""))
