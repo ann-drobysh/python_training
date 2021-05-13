60e271fcf70d5d996f47fb7382874cbdee9e7f18
Anna Drobysh <ann.drobysh@yandex.by>
2021-05-13 18:57:35 +0200

    Optimize navigation.

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,7 +6,8 @@ class ContactHelper:
 
     def open_add_contact_screen(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
 
     def add_new_contact(self, contact):
@@ -150,7 +151,8 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
