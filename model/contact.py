f961ed6018a391d3e6639429a64f2754c6b4f5ae
Anna Drobysh <ann.drobysh@yandex.by>
2021-05-30 17:27:41 +0200

    Parametrization of "Add group" and "Add contact" tests

--- model/contact.py
+++ model/contact.py
@@ -35,7 +35,9 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.middlename, self.street,
+                                self.homephone, self.mobilephone, self.email, self.address2, self.phone2)
+
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
